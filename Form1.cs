a2a7fd055dc5aeec00134525bb474a4c86c803d4
romanzedd <r.zayko@gmail.com>
2018-06-25 12:06:48 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace fileEncryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                        label2.Text = openFileDialog1.FileName;
+                        label2.Visible = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+        private static void AES_Encrypt(string inputFile, string outputFile, byte[] passwordBytes)
+        {
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            string cryptFile = outputFile;
+            FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+            RijndaelManaged AES = new RijndaelManaged();
+
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.Zeros;
+
+            AES.Mode = CipherMode.CBC;
+
+            CryptoStream cs = new CryptoStream(fsCrypt,
+                 AES.CreateEncryptor(),
+                CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            int data;
+            while ((data = fsIn.ReadByte()) != -1)
+                cs.WriteByte((byte)data);
+
+
+            fsIn.Close();
+            cs.Close();
+            fsCrypt.Close();
+
+        }
+
+        private static void AES_Decrypt(string inputFile, string outputFile, byte[] passwordBytes)
+        {
+
+
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+            RijndaelManaged AES = new RijndaelManaged();
+
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.Zeros;
+
+            AES.Mode = CipherMode.CBC;
+
+            CryptoStream cs = new CryptoStream(fsCrypt,
+                AES.CreateDecryptor(),
+                CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int data;
+            while ((data = cs.ReadByte()) != -1)
+                fsOut.WriteByte((byte)data);
+
+            fsOut.Close();
+            cs.Close();
+            fsCrypt.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes("password");
+            AES_Encrypt(label2.Text, label2.Text + ".map", passwordBytes);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes("password");
+            AES_Decrypt(label2.Text, label2.Text + ".dec", passwordBytes);
+        }
+    }
+}
